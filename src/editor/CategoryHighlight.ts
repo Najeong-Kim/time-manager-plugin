c73cb68bc4ba59e45e39a1451581551f4c0e5123
Najeong Kim <atpeoe1023@gmail.com>
2026-05-13 01:22:39 +0900

    Fix timeline UX: initial load, date sync, editor highlights, stats bar, sticky layout

--- src/editor/CategoryHighlight.ts
+++ src/editor/CategoryHighlight.ts
@@ -1,54 +1,51 @@
-import { ViewPlugin, DecorationSet, ViewUpdate, Decoration } from "@codemirror/view";
+import { ViewPlugin, DecorationSet, ViewUpdate, Decoration, EditorView } from "@codemirror/view";
 import { RangeSetBuilder } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
 import { CategoryConfig } from "../types";
 
 const CAT_RE = /@(work|personal|hobby|study)\b/g;
 const TIME_RE = /^- \[[ x]\] (\d{2}:\d{2})/gm;
-const DUR_RE = /^- \[[ x]\].*?\s(\d+h\d+m|\d+h|\d+m)\s*$/gm;
+const DUR_RE = /^- \[[ x]\].*?\s(\d+h\d+m|\d+h|\d+m)[ \t]*$/gm;
 
 const TIME_MARK = Decoration.mark({ attributes: { class: "tm-editor-time" } });
 const DUR_MARK  = Decoration.mark({ attributes: { class: "tm-editor-dur" } });
 
 function buildDecorations(view: EditorView, categories: CategoryConfig[]): DecorationSet {
   const builder = new RangeSetBuilder<Decoration>();
+  const text = view.state.doc.toString();
+  const marks: { start: number; end: number; deco: Decoration }[] = [];
 
-  for (const { from, to } of view.visibleRanges) {
-    const text = view.state.doc.sliceString(from, to);
-    const marks: { start: number; end: number; deco: Decoration }[] = [];
+  TIME_RE.lastIndex = 0;
+  let m: RegExpExecArray | null;
+  while ((m = TIME_RE.exec(text)) !== null) {
+    const start = m.index + m[0].length - m[1].length;
+    marks.push({ start, end: start + m[1].length, deco: TIME_MARK });
+  }
 
-    TIME_RE.lastIndex = 0;
-    let m: RegExpExecArray | null;
-    while ((m = TIME_RE.exec(text)) !== null) {
-      const start = from + m.index + m[0].length - m[1].length;
-      marks.push({ start, end: start + m[1].length, deco: TIME_MARK });
-    }
+  DUR_RE.lastIndex = 0;
+  while ((m = DUR_RE.exec(text)) !== null) {
+    const start = m.index + m[0].length - m[1].length;
+    marks.push({ start, end: start + m[1].length, deco: DUR_MARK });
+  }
 
-    DUR_RE.lastIndex = 0;
-    while ((m = DUR_RE.exec(text)) !== null) {
-      const start = from + m.index + m[0].length - m[1].length;
-      marks.push({ start, end: start + m[1].length, deco: DUR_MARK });
-    }
-
-    CAT_RE.lastIndex = 0;
-    while ((m = CAT_RE.exec(text)) !== null) {
-      const start = from + m.index;
-      const end = start + m[0].length;
-      const catCfg = categories.find((c) => c.id === m![1]);
-      if (catCfg) {
-        marks.push({
-          start,
-          end,
-          deco: Decoration.mark({ attributes: { style: `color: ${catCfg.color}; font-weight: 600;` } }),
-        });
-      }
-    }
-
-    marks.sort((a, b) => a.start - b.start);
-    for (const { start, end, deco } of marks) {
-      builder.add(start, end, deco);
+  CAT_RE.lastIndex = 0;
+  while ((m = CAT_RE.exec(text)) !== null) {
+    const start = m.index;
+    const end = start + m[0].length;
+    const catCfg = categories.find((c) => c.id === m![1]);
+    if (catCfg) {
+      marks.push({
+        start,
+        end,
+        deco: Decoration.mark({ attributes: { style: `color: ${catCfg.color}; font-weight: 600;` } }),
+      });
     }
   }
+
+  marks.sort((a, b) => a.start - b.start);
+  for (const { start, end, deco } of marks) {
+    builder.add(start, end, deco);
+  }
+
   return builder.finish();
 }
 
